c1c7de56b9a19cdaf60a272b51613d826216b361
Valentin <valentin.lucet@gmail.com>
2021-05-06 12:08:17 -0400

    modified aspirationnaly R script

---
+++ 1_download_forecasts_IHME.R
@@ -0,0 +1,40 @@
+# HUB ---------------------------------------------------------------------
+
+library(rsyncrosim)
+library(COVID19)
+E <- ssimEnvironment()
+TRANSFORMER_NAME <- "Download IHME forecasts"
+
+# Source helpers ----------------------------------------------------------
+
+source(file.path(E$PackageDirectory, "0_epiDataForecastIHME_env.R"))
+source(file.path(E$PackageDirectory, "0_epiDataForecastIHME_functions.R"))
+source(file.path(E$PackageDirectory, "0_HIME_functions.R"))
+
+# 1. Load data
+
+inputs <- load_inputs(backend = "IHME", mySce = SCE, e = E)
+
+# 2. Save to epi
+
+save_to_epi(dataSubset = inputs$IHMEData, mySce = SCE, vars = VARS)
+
+# 3. Process data and save it
+
+forecasts <- process_data(
+    data = inputs$IHMEData,
+    backend = "IHME", lookup = LOOKUP) %>%
+    mutate(TransformerID = TRANSFORMER_NAME)
+
+saveDatasheet(SCE, covidDataFinal, "epi_DataSummary", append = TRUE)
+
+# 4. Write out data
+
+fileName <- make_filename(inputs_vars = inputs$input_vars)
+filePath <- file.path(E$TransferDirectory, fileName)
+
+write.csv(forecasts, filePath, row.names = FALSE)
+
+# 5. Save outpout
+
+save_output_info(mySce = SCE, input_vars = inputs$input_vars, backend = "HUB", filePath = filePath)
