40266815dd45134a34380429770e02e62d3a7dca
vlucet <VLucet@users.noreply.github.com>
2021-05-06 22:02:43 +0000

    add new packages to load

--- 0_epiForecastIHME_env.R
+++ 0_epiForecastIHME_env.R
@@ -1,27 +1,35 @@
 # Environment -------------------------------------------------------------
 
 # Load libraries
+library(rsyncrosim)
 library(dplyr)
 library(tidyr)
 library(readr)
 library(lubridate) 
+library(httr)
+library(stringr)
+library(rvest)
 
-# Load environment
+# Load environment --------------------------------------------------------
 
 LIB <- ssimLibrary()
 SCE <- scenario()
 
-# TRANSFER_DIR <- e$TransferDirectory
+# Variables ---------------------------------------------------------------
+
+IHME_URL <- "http://www.healthdata.org/covid/data-downloads"
 
 VARS <- c("Cases - Cumulative", "Cases - Daily", 
           "Recovered - Cumulative", "Recovered - Daily", 
           "Tested - Cumulative", "Tested - Daily",
           "Deaths - Cumulative", "Deaths - Daily", 
           "Vaccines - Cumulative", "Vaccines - Daily")
+
 RAWVARS <- c("confirmed", "dailyconfirmed", 
              "recovered", "dailyrecovered", 
              "tested", "dailytested", 
              "deaths", "dailydeaths", 
              "vaccines", "dailyvaccines")
+
 LOOKUP <- data.frame(VARS = VARS,
                      RAWVARS = RAWVARS)
