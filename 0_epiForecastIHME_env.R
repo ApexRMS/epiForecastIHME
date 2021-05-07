a882eb844fbc54954e18be32fe135fd56551fa38
vlucet <VLucet@users.noreply.github.com>
2021-05-07 16:11:15 +0000

    add more env vars

--- 0_epiForecastIHME_env.R
+++ 0_epiForecastIHME_env.R
@@ -14,22 +14,34 @@ library(rvest)
 
 LIB <- ssimLibrary()
 SCE <- scenario()
+E <- ssimEnvironment()
 
 # Variables ---------------------------------------------------------------
 
+TRANSFORMER_NAME <- "Download IHME forecasts"
+
 IHME_URL <- "http://www.healthdata.org/covid/data-downloads"
 
-VARS <- c("Cases - Cumulative", "Cases - Daily", 
-          "Recovered - Cumulative", "Recovered - Daily", 
-          "Tested - Cumulative", "Tested - Daily",
-          "Deaths - Cumulative", "Deaths - Daily", 
-          "Vaccines - Cumulative", "Vaccines - Daily")
+VARS <- 
+  c("Hospitalizations - Daily - Mean", "Hospitalizations - Daily - Min", "Hospitalizations - Daily - Max", 
+    "Deaths - Daily - Mean", "Deaths - Daily - Min", "Deaths - Daily - Max", 
+    "Deaths - Cumulative - Mean", "Deaths - Cumulative - Min", "Deaths - Cumulative - Max", 
+    "Infections - Daily - Mean", "Infections - Daily - Min", "Infections - Daily - Max", 
+    "Infections - Cumulative - Mean", "Infections - Cumulative - Min", "Infections - Cumulative - Max")
 
-RAWVARS <- c("confirmed", "dailyconfirmed", 
-             "recovered", "dailyrecovered", 
-             "tested", "dailytested", 
-             "deaths", "dailydeaths", 
-             "vaccines", "dailyvaccines")
+RAWVARS <- 
+  c("admis_mean", "admis_lower", "admis_upper",
+    "deaths_mean_smoothed", "deaths_lower_smoothed", "deaths_upper_smoothed", 
+    "totdea_mean_smoothed", "totdea_lower_smoothed", "totdea_upper_smoothed", 
+    "est_infections_mean", "est_infections_lower", "est_infections_upper", 
+    "inf_cuml_mean", "inf_cuml_lower", "inf_cuml_upper")
 
-LOOKUP <- data.frame(VARS = VARS,
-                     RAWVARS = RAWVARS)
+VARS_LOOKUP <- data.frame(VARS = VARS,
+                          RAWVARS = RAWVARS)
+
+JURIS_LOOkUP <- read_csv(file.path(E$PackageDirectory, "data", 
+                                   "juris_lookup.csv"), col_types = cols(
+                                     administrative_area_level_1 = col_character(),
+                                     administrative_area_level_2 = col_character(),
+                                     administrative_area_level_3 = col_character()
+                                   ))
